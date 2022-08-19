0685c46e006c4a58cd742c96ee791eeada3c2d21
ROHIT <ROHIT@DESKTOP-8SI0GBB>
2022-08-20 02:38:01 +0530

    first commit changes staging

---
+++ src/classs.java
@@ -0,0 +1,12 @@
+
+public class classs {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+        System.out.println(" Practise Core Java ");
+		
+		System.out.println(" Practise Core Java updated checked ");
+	}
+
+}
